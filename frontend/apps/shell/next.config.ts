e5b527c85fc11baaf8d6128dff45b9de5958562c
vignesh <avmvignesh0207@gmail.com>
2026-03-16 15:29:40 +0530

    feat: Add numerous new UI components across various ERP modules and introduce new micro-frontend applications.

---
+++ frontend/apps/shell/next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  transpilePackages: [
+    "@repo/ui",
+    "@repo/shared",
+    "@repo/mfe-auth",
+    "@repo/b2b-services"
+  ],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  }
+};
+
+export default nextConfig;
